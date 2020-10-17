a084fd1b231c68fcccab59ff087ea7b36ca7823c
DatDonathan <jojo08082003@gmail.com>
2020-10-17 15:54:36 +0200

    Added Colossus AI and spawn command

--- src/at/minesouls/MineSouls.java
+++ src/at/minesouls/MineSouls.java
@@ -3,12 +3,18 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import at.jojokobi.mcutil.entity.spawns.CustomSpawnsHandler;
+import at.jojokobi.mcutil.entity.spawns.FunctionSpawn;
+import at.minesouls.entity.WalkingColossusBoss;
+
 public class MineSouls extends JavaPlugin {
     @Override
     public void onEnable() {
         super.onEnable();
         getServer().getPluginManager().registerEvents(new MineSoulsListener(), this);
         Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + " [MINESOULS LOADED]");
+        
+        CustomSpawnsHandler.getInstance().addItem(new FunctionSpawn(getName().toLowerCase(), "walking_colossus", l -> new WalkingColossusBoss(l, null)));
     }
 
     @Override
--- src/at/minesouls/entity/WalkingColossusBoss.java
+++ src/at/minesouls/entity/WalkingColossusBoss.java
@@ -6,18 +6,24 @@ import org.bukkit.Location;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
+import org.bukkit.entity.Damageable;
 import org.bukkit.entity.IronGolem;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import at.jojokobi.mcutil.entity.Attacker;
 import at.jojokobi.mcutil.entity.BossBarComponent;
 import at.jojokobi.mcutil.entity.CustomEntity;
 import at.jojokobi.mcutil.entity.EntityHandler;
 import at.jojokobi.mcutil.entity.EntityMapData;
 import at.jojokobi.mcutil.entity.HealthComponent;
+import at.jojokobi.mcutil.entity.NMSEntityUtil;
 import at.jojokobi.mcutil.entity.RealHealthAccessor;
+import at.jojokobi.mcutil.entity.ai.AttackTask;
+import at.jojokobi.mcutil.entity.ai.RandomTask;
 import at.minesouls.MineSouls;
 
-public class WalkingColossusBoss extends CustomEntity<IronGolem> {
+public class WalkingColossusBoss extends CustomEntity<IronGolem> implements Attacker {
 	
 	private HealthComponent health;
 	private BossBarComponent bossBar;
@@ -27,6 +33,10 @@ public class WalkingColossusBoss extends CustomEntity<IronGolem> {
 		health = new HealthComponent(new RealHealthAccessor());
 		addComponent(health);
 		bossBar = new BossBarComponent("Walking Colossus", BarColor.RED, BarStyle.SEGMENTED_6);
+		addComponent(bossBar);
+		
+		addEntityTask(new AttackTask(Player.class));
+		addEntityTask(new RandomTask());
 	}
 	
 	@Override
@@ -44,6 +54,9 @@ public class WalkingColossusBoss extends CustomEntity<IronGolem> {
 	protected IronGolem createEntity(Location place) {
 		IronGolem golem = place.getWorld().spawn(place, IronGolem.class);
 		golem.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(120);
+		golem.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE).setBaseValue(1);
+		
+		NMSEntityUtil.clearGoals(golem);
 		
 		return golem;
 	}
@@ -56,6 +69,27 @@ public class WalkingColossusBoss extends CustomEntity<IronGolem> {
 	@Override
 	protected EntityMapData saveData() {
 		return new EntityMapData(new HashMap<>());
+	}
+
+	@Override
+	public void attack(Damageable entity) {
+		entity.damage(5);
+		entity.setVelocity(entity.getVelocity().setY(1.5));
+	}
+
+	@Override
+	public int getAttackDelay() {
+		return 8;
+	}
+	
+	@Override
+	protected double getWalkSpeed() {
+		return 0.2;
+	}
+	
+	@Override
+	protected double getSprintSpeed() {
+		return 0.8;
 	}
 
 }
